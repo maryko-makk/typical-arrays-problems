e10b90e3e79d9666137db03495aeae4a7e1d66c6
maryko-makk <maryko-makk@yandex.ru>
2021-03-04 10:50:17 +0300

    add functions body

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (!array || array == '') {
+    return 0;
+  } else {
+    let minimum = array[0];
+    for (let i=1; i<array.length; i++) {
+      if (array[i] < minimum) {
+        minimum = array[i];
+      }
+    } 
+    return minimum;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array == '') {
+    return 0;
+  } else {
+    let maximum = array[0];
+    for (let i=1; i<array.length; i++) {
+      if (array[i] > maximum) {
+        maximum = array[i];
+      }
+    } 
+    return maximum;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array == '') {
+    return 0;
+  } else {
+    let sum = 0;
+    for (let i=0; i<array.length; i++) {
+      sum+= array[i];
+    }
+    return (sum / array.length);
+  }
 }
